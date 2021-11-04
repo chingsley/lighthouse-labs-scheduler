fa19db1b7fa69d14a9e37d123ba4a5711c22d7b3
chingsley <eneja.kc@gmail.com>
2021-11-04 15:49:08 +0100

    - Implement and test the useVisualMode custom hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replaceLastMode = false) {
+    if (replaceLastMode) {
+      setHistory((prev) => [...prev.slice(0, history.length - 1), newMode]);
+    } else {
+      setHistory([...history, newMode]);
+    }
+  }
+  function back() {
+    // if there's only item in history, then we no mode to go back to
+    // therefore, history must have a minimum of 1 item at every point time
+    // ... since mode = last item in history, if history is empty then mode would be undefined...
+    // ... and we don't want that
+    if (history.length < 2) return;
+
+    // remove last history item
+    setHistory((prev) => [...prev.slice(0, history.length - 1)]);
+    console.log('history =', history);
+  }
+
+  return {
+    mode: history[history.length - 1],
+    transition,
+    back,
+  };
+}
