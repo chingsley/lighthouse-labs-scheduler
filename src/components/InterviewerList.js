e0b764e8c885d62369184d4f43e813ca29c14212
chingsley <eneja.kc@gmail.com>
2021-12-01 09:38:46 +0100

    - Add prop type for prop validation

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import InterviewerListItem from './InterviewerListItem';
 
 import 'components/InterviewerList.scss';
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   const { selectedInterviewerId, interviewers, setSelectedInterviewerId } =
     props;
 
@@ -26,3 +27,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
+
+export default InterviewerList;
