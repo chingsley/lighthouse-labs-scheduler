d5d98933989b9a2ce0f378afa0d13a9df362542b
Kingsley Eneja <kingsley.eneja@getir.com>
2023-04-26 21:54:05 +0100

    Test: 'should navigate to Tuesday'

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,10 +3,28 @@ describe('Navigation', () => {
     cy.visit('/');
   });
 
+  // OR
   it('should navigate to Tuesday', () => {
     cy.visit('/');
     cy.contains('[data-testid=day]', 'Tuesday')
       .click()
       .should('have.class', 'day-list__item--selected');
   });
+
+  // // OR
+  // it('should navigate to Tuesday', () => {
+  //   cy.visit('/');
+  //   cy.get("li").contains("Tuesday").click();
+  //   cy.contains("li", "Tuesday")
+  //     .should("have.css", "background-color", "rgb(242, 242, 242)");
+  // });
+
+  // // OR
+  // it('should navigate to Tuesday', () => {
+  //   cy.visit('/');
+
+  //   cy.contains("li", "Tuesday")
+  //     .click()
+  //     .should("have.css", "background-color", "rgb(242, 242, 242)");
+  // });
 });
